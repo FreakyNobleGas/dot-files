d530327a706db5b383a6c44a2bdc884cd1041aa6
freakynoblegas <nickquinn3131@gmail.com>
2025-11-05 16:10:48 -0500

    Update dot files

---
+++ scripts/update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# Script Description: Update configuration files in the repository
+#
+
+# Save current dir and move to script directory
+pushd "$(dirname "$0")" > /dev/null
+
+# Save .oh-my-zsh custom plugins and themes
+rsync -av --exclude 'plugins' --exclude 'themes' "$HOME/.oh-my-zsh/custom/" ".oh-my-zsh/custom/"
+
+# Save .zshrc
+cp $HOME/.zshrc .zshrc
+
+# Save Ghostty Config file
+cp $HOME/.config/ghostty/config .config/ghostty/config
+
+# Commit and push changes to GitHub
+git add .
+git commit -m "Update dot files"
+git push origin main 
+
+# Return to original directory
+popd > /dev/null
