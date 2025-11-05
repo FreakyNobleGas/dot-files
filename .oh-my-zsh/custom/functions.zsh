16f6271a1e9d990cb1efdcc26c6ccd3f9ad13d21
freakynoblegas <nickquinn3131@gmail.com>
2025-11-05 16:23:44 -0500

    Update dot files

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -3,9 +3,7 @@ GITHUB_PATH="$HOME/Github/"
 
 # DOT FILES
 export save_dot_files() {
-  rsync -av --exclude 'plugins' --exclude 'themes' "$HOME/.oh-my-zsh/custom/" "$GITHUB_PATH/dot-files/.oh-my-zsh/custom/"
-  # cp -r $HOME/.oh-my-zsh/custom $GITHUB_PATH/dot-files/.oh-my-zsh/custom
-  cp $HOME/.zshrc $GITHUB_PATH/dot-files/.zshrc
+  bash ${GITHUB_PATH}dot-files/scripts/update.sh
 }
 
 # GITHUB
