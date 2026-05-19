0253608336dae935b20b43b8b12574cde4c1041e
freakynoblegas <nickquinn3131@gmail.com>
2026-05-19 16:07:30 -0400

    cleaned up unused packages

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -15,17 +15,6 @@ brew install openjdk # For Groovy syntax support in NeoVim
 # rbenv global <version>
 brew install rbenv ruby-build
 
-# Install Language Servers for NeoVim
-brew install lua-language-server
-brew install typescript-language-server
-brew install pyright
-brew install yaml-language-server
-brew install vscode-langservers-extracted # HTML, CSS, JSON Language Servers
-
-gem install solargraph # Ruby Language Server
-
-npm install -g svelte-language-server
-npm install -g dockerfile-language-server-nodejs
 
 # Install dependencies that I use on my personal machine
 if [ "$USER" == "freakynoblegas" ]; then
